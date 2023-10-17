043392a59ef77a17a652b9266c5e69e03f5d205d
Egor Yolkin <egoryolkin@MacBook-Air-Egor.local>
2023-10-17 20:32:28 +0500

    Fix eval in calculate expressions

--- services/inline_service.py
+++ services/inline_service.py
@@ -1,10 +1,18 @@
 from helpers.flags import extract_flags
 
 
+def calculate(expression):
+    try:
+        return eval(expression, {'builtins': None}, {})
+    except:
+        return "Ошибка при вычислении"
+
+
+
 def get_expression_result(query_text: str) -> str:
     query_text, flags = extract_flags(query_text)
     
-    expression_result = str(eval(query_text))
+    expression_result = str(calculate(query_text))
     
     if "h" not in flags:
         expression_result = f"{query_text} = {expression_result}"
